c05a92452af1aa1609e2646e1387c11d206c1ddc
kouji tanaka <t.kouji1281@gmail.com>
2020-03-29 14:26:54 +0900

    profile.html、edit.html作成完了。app.pyへdef update_profileを作成する前段。

--- _app_.py
+++ _app_.py
@@ -1,7 +1,18 @@
 from flask  import Flask,render_template,request
+import json
 
 #アプリオブジェクトの作成
 app = Flask(__name__)
+
+def get_profile():
+    #jsonファイルの書き込み
+    file_json = "data/profile.json"
+    with open(file_json,encoding='utf-8') as prof:
+        json_str = prof.read()
+    
+    #json形式から辞書型へ変換
+    prof_dict = json.loads(json_str)
+    return prof_dict
 
 #ルーティング
 @app.route('/')
@@ -9,9 +20,9 @@ def hello():
     name = "Hello World"
     return name
 
-@app.route('/profile')
-def profile():
-    return "name:Kouji Tanaka  address:Tokyo"
+# @app.route('/profile')
+# def profile():
+#     return "name:Kouji Tanaka  address:Tokyo"
 
 @app.route('/hive')
 def hive_func():
@@ -37,6 +48,16 @@ def get():
     #GETリクエストを格納。nにGETで取得した値を格納し、get.htmlの{{na}}に渡す。
     n = request.args.get("name") #.get("hoge")ここのhogeは『URL?パラメータ名=値』の所のパラメータ名の部分。
     return render_template('get.html',title='Flask GET request!',na = n)
+
+@app.route('/profile')
+def profile():
+    prof_dict = get_profile()
+    return render_template('profile.html',title= "json",user=prof_dict)
+
+@app.route('/edit')
+def edit():
+    prof_dict = get_profile()
+    return render_template('edit.html',title= "json")
 
 if __name__ == "__main__":
     app.run(debug=True)
